03b072fc160c20a16e55dbe931ed86b6f2d68998
GGICE <i@ice.gs>
2018-11-05 20:37:34 +0800

    fix: Not support async function #2

--- client/sendLog.ts
+++ client/sendLog.ts
@@ -2,8 +2,5 @@ import * as http from 'http';
 import { env } from 'vscode';
 
 export function sendLog () {
-  if (process.env['VSCODE_DEV']) {
-    return;
-  }
   http.get(`http://gm.mmstat.com/esc.main.vscode-weex-lang?mid=${env.machineId}&t=${Date.now()}`);
 }
